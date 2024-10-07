c8b5eddd9450b4eabd5cfd7a9c4e0e9090373df4
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-07 15:05:44 +0200

    added form

--- index.php
+++ index.php
@@ -14,6 +14,14 @@
 <body>
     <h1>Strong Password Generator</h1>
     <h2>Genera una password sicura</h2>
+
+    <!--Inizio Form-->
+    <form action="index.php" method="GET">
+        <label for="pwlength">Lunghezza Password:</label>
+        <input type="number" name="pwlength" id="pwlength">
+        <button type="submit">Invia</button>
+        <button type="reset">Annulla</button>
+    </form>
 </body>
 
 </html>
