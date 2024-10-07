80610b230a489b8ceebc585bc8d8db9d18ad22ae
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-07 15:40:51 +0200

    commentend bin2hex

--- index.php
+++ index.php
@@ -5,10 +5,59 @@ $letters = [
     "b",
     "c",
     "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
 ];
 
 function getPassword ($length) {
-   return bin2hex(random_bytes($length));
+   //return bin2hex(random_bytes($length));
+    
 }
 
 ?>
@@ -25,7 +74,7 @@ function getPassword ($length) {
 <body>
     <h1>Strong Password Generator</h1>
     <h2>Genera una password sicura</h2>
-
+    <?php  ?>
     <!--Inizio Form-->
     <form action="index.php" method="GET">
         <label for="pwlength">Lunghezza Password:</label>
